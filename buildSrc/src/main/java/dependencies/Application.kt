23358e22646a93cc321e541252a92cda8b88315d
Shalom <shalommathews05@gmail.com>
2020-08-01 15:16:11 -0600

    buildSrc added

---
+++ buildSrc/src/main/java/dependencies/Application.kt
@@ -0,0 +1,9 @@
+package dependencies
+
+object Application {
+    const val id = "com.shalom.marvelogy"
+    const val version_code = 1
+    const val version_name = "1.0"
+    const val min_version=23
+    const val target_version=29
+}
---
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -0,0 +1,45 @@
+package dependencies
+
+
+object Dependencies {
+    val kotlin_standard_library = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    val kotlin_reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+    val ktx = "androidx.core:core-ktx:${Versions.ktx}"
+    val kotlin_coroutines =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines_version}"
+    val kotlin_coroutines_android =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines_version}"
+    val kotlin_coroutines_play_services =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:${Versions.coroutines_play_services}"
+    val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    val navigation_fragment =
+        "androidx.navigation:navigation-fragment-ktx:${Versions.nav_components}"
+    val navigation_runtime = "androidx.navigation:navigation-runtime:${Versions.nav_components}"
+    val navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.nav_components}"
+    val navigation_dynamic =
+        "androidx.navigation:navigation-dynamic-features-fragment:${Versions.nav_components}"
+
+
+    val room_runtime = "androidx.room:room-runtime:${Versions.room}"
+    val room_ktx = "androidx.room:room-ktx:${Versions.room}"
+    val play_core = "com.google.android.play:core:${Versions.play_core}"
+    val leak_canary = "com.squareup.leakcanary:leakcanary-android:${Versions.leak_canary}"
+    val firebase_firestore =
+        "com.google.firebase:firebase-firestore-ktx:${Versions.firebase_firestore}"
+
+    val firebase_analytics =
+        "com.google.firebase:firebase-analytics-ktx:${Versions.firebase_analytics}"
+    val firebase_crashlytics =
+        "com.google.firebase:firebase-crashlytics:${Versions.firebase_crashlytics}"
+    val lifecycle_runtime = "androidx.lifecycle:lifecycle-runtime:${Versions.lifecycle_version}"
+    val lifecycle_coroutines =
+        "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle_version}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit2_version}"
+    val retrofit_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit2_version}"
+
+
+    val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    val constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}"
+    val material_design = "com.google.android.material:material:${Versions.material_design}"
+
+}
