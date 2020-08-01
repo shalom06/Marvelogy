20f1a9a4a97a4bf9e5e5970d005d40047b0dbd36
Shalom <shalommathews05@gmail.com>
2020-08-01 15:16:28 -0600

    buildSrc added

---
+++ buildSrc/src/main/java/dependencies/Java.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object Java {
+
+    const val java_version = "1.8"
+}
---
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -0,0 +1,41 @@
+package dependencies
+
+object Versions {
+
+    val gradle = "3.5.3"
+    val compilesdk = 29
+    val minsdk = 23
+    val targetsdk = 29
+    val kotlin = "1.3.72"
+    val ktx = "1.3.0"
+    val dagger = "2.25.4"
+    val nav_components = "2.3.0"
+
+    val room = "2.2.5"
+    val appcompat = "1.2.0-rc02"
+    val constraintlayout = "2.0.0-beta8"
+    val material_design = "1.2.0-rc01"
+    val play_core = "1.7.3"
+    val play_services = "4.3.3"
+    val leak_canary = "2.4"
+
+    val firebase_firestore = "21.4.2"
+    val firebase_analytics = "17.4.4"
+    val firebase_crashlytics = "17.1.1"
+    val firebase_auth = "19.3.0"
+    val espresso_core = "3.1.1"
+    val espresso_idling_resource = "3.2.0"
+    val mockk_version = "1.9.2"
+    val test_runner = "1.2.0"
+    val test_core = "1.2.0"
+    val coroutines_version = "1.3.8"
+    val coroutines_play_services = "1.3.8"
+    val lifecycle_version = "2.2.0"
+    val retrofit2_version = "2.9.0"
+    val markdown_processor = "0.1.3"
+    val junit_jupiter_version = "5.6.0"
+    val junit_4_version = "4.12"
+    val fragment_version = "1.2.0"
+    val androidx_test_ext = "1.1.1"
+    val crashlytics_gradle = "2.1.0"
+}
